47b19579c614b85e12e125f99e21dcb383643e17
mariacarreno <38597200+mariacarreno@users.noreply.github.com>
2019-01-29 19:30:52 -0500

    Create my first PHP FILE

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mi primer codigo de PHP</title>    
+</head>
+<body>
+    <h1>Hola Mundo</h1>
+    <?php
+      echo "<p>Gracias por esto :)</p>";
+      echo "<p style='color:blue'>Luego aprendo porque ahora estoy ocupada</p>";
+  //mi primer comentario
+  #otro comentario
+  /*
+    Un bloque de comentario grande
+    :)
+  */
+/*
+  // practica
+  print "Hola mundo";
+  echo "Mi primer Hola Mundo con ed Team";
+  echo "Hola ", "mundo";
+
+  echo "<h1>Hola a todos</h1>";
+  */
+?>
+</body>
+</html>
